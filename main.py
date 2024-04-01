6a13e23c7b4655babd382f78cc0b85a3097f0330
Buddyroo <buddyroo@yandex.ru>
2024-04-01 20:53:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 
-print("Conflict")
-        def on_date_selected():
-            selected_date = cal.selection_get()
-            task_listbox.set(selected_task_index, column=2, value=selected_date)
-            top.destroy()
-
-        top = Toplevel(root)
-        top.grab_set()  # pop-up window of the calender
-
-        cal = Calendar(top, selectmode='day', day=4, month=3, year=2024)
-        cal.pack(pady=20)
-
-        ok_button = tk.Button(top, text="OK", command=on_date_selected)
-        ok_button.pack()
+print("Conflict resolved")
+   
 
 
