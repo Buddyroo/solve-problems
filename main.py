a1c8ab4cadff13b359e2de21dc78c50a9102ef31
Buddyroo <buddyroo@yandex.ru>
2024-04-01 20:44:29 -0300

    added code

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-a=5
+def choose_date():
+    selected_task_index = task_listbox.selection()
+    if selected_task_index:
+        def on_date_selected():
+            selected_date = cal.selection_get()
+            task_listbox.set(selected_task_index, column=2, value=selected_date)
+            top.destroy()
+
+        top = Toplevel(root)
+        top.grab_set()  # pop-up window of the calender
+
+        cal = Calendar(top, selectmode='day', day=4, month=3, year=2024)
+        cal.pack(pady=20)
+
+        ok_button = tk.Button(top, text="OK", command=on_date_selected)
+        ok_button.pack()
 
 
