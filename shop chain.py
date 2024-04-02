f40cd5b82516ad04e9f303e1a9422e731f973d29
Buddyroo <buddyroo@yandex.ru>
2024-04-02 18:04:27 -0300

    Update shop chain.py

--- shop chain.py
+++ shop chain.py
@@ -26,56 +26,59 @@
 
 class Store:
 
-    def __init__(self, name, address, items={}):
+    def __init__(self, name, address, items=None):
         self.name = name
         self.address = address
-        self.items = items
+        if items == None:
+            self.items = {}
+        else:
+            self.items = { key.lower() : float(value) for key, value in items.items()}
 
-    def add_items(self,list={}):
+    def add_items(self,list=None):
         if list!=None:
-            updated_list= { key.lower(): value for key , value in list.items()}
+            updated_list= { key.lower(): float(value) for key , value in list.items()}
             self.items.update(updated_list)
-
+        else:
+            list = {}
+        print(f"\nЧтобы добавить товарные позиции в магазин '{self.name}' по адресу {self.address},\n"
+                          f"введите название продукта и цену, через пробел. Когда закончите, введите 'Стоп'.\n")
         while True:
             try:
-                a = input(f"Чтобы добавить товарные позиции в магазин '{self.name}' по адресу {self.address},\n"
-                          f"введите название продукта и цену, через пробел. Когда закончите, введите 'Стоп'.\n")
-                if a == "Стоп":
+                a = input()
+                if a.lower() == "стоп":
                     break
                 else:
                     a = a.split()
-                    print(a)
                     self.items[a[0].lower()]=float(a[1])
-                    print (self.items)
-                    print()
             except ValueError:
                     print("---ОШИБКА: В поле 'Цена' должно быть введено число---")
             except Exception as e:
-                print(f"---ОШИБКА: {e}---")
+                print(f"---ОШИБКА: Введите название и цену через пробел---")
 
     def delete_items(self,product):
         deleted = self.items.pop(product.lower(),None)
         if deleted != None:
-            print(f"Товар {product} был удален из ассортимента магазина {self.name}")
+            print(f"\nТовар {product} был удален из ассортимента магазина {self.name}")
         else:
-            print("Такого товара в магазине нет")
+            print(f"\nТовара {product} нет в магазине {self.name}")
 
     def show_price(self, product):
         price = self.items.get(product.lower(),None)
         if price != None:
-            print(f"В магазине {self.name} цена товара {product}: {price}")
+            print(f"\nВ магазине {self.name} цена товара {product}: {price}")
         else:
-            print(f"Такого товара в магазине {self.name} нет")
+            print(f"\nТовара {product} в магазине {self.name} нет")
 
     def price_update(self, product, price):
-        if product.lower not in self.items.keys():
-            print(f"Товара {product} нет в магазине {self.name}")
+        if product.lower() not in self.items.keys():
+            print(f"\nТовара {product} нет в магазине {self.name}")
         else:
-            self.items[product.lower()]=price
-            print (f"В магазине {self.name} новая цена товара {product}: {price}")
+            self.items[product.lower()]= float(price)
+            print (f"\nВ магазине {self.name} новая цена товара {product}: {float(price)}")
 
     def list_products(self ):
-        print(f"В ассортименте магазина {self.name} находятся следующие товары")
+        print(f"\nВ ассортименте магазина {self.name} находятся следующие товары")
+        print(self.items)
         i=1
         for product,price in self.items.items():
             print(f"{i}. Товар - {product}, цена - {price}")
@@ -87,19 +90,25 @@ class Store:
 store1 = Store("Цветочный", "Юбилейный проспект, 10")
 store1.add_items({"Пион":20,"Тюльпан":30})
 store1.list_products()
+
 store2 = Store("Продукты", "Улица Ленина, 10")
 store2.add_items()
+store2.list_products()
+
 store3 = Store("Игрушки", "Улица Маяковского, 15")
 store3.add_items()
+store3.list_products()
+
 store1.delete_items("пион")
 store1.list_products()
+
 store1.delete_items("яблоко")
-store1.show_price("тюльпан")
-store1.delete_items("яблоко")
+
 store1.show_price("тюльпан")
 store1.show_price("яблоко")
-store1.update_price("тюльпан")
-store1.update_price("яблоко")
+
+store1.price_update("тюльпан", 50)
+store1.price_update("яблоко", 40)
 
 
 
